67aa88f140c654300e0b27a6e02ca7009701543d
Saori Isikawa <saori.isikawa414@gmail.com>
2024-07-21 17:11:07 +0800

    更新

--- bin/update.sh
+++ bin/update.sh
@@ -4,7 +4,7 @@ sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 sudo snap refresh
 sudo flatpak -y update
 #fc-cache -fv
-sudo fc-cache -fv
+#sudo fc-cache -fv
 #sudo update-initramfs -c -k "$(uname -r)"
 #sudo update-grub
 
