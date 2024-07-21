03ec57e24563ba30b0be5da078bdfa47a5954c14
Saori Isikawa <saori.isikawa414@gmail.com>
2024-07-21 16:16:33 +0800

    更新

--- bin/update.sh
+++ bin/update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #sudo ntpd -qg && sudo hwclock -w -v
-sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y autoremove
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 sudo snap refresh
 sudo flatpak -y update
 #fc-cache -fv
-#sudo fc-cache -fv
+sudo fc-cache -fv
 #sudo update-initramfs -c -k "$(uname -r)"
 #sudo update-grub
 
