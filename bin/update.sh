2c690e7c0652706ea9784f4c8b62a10749c07e4e
Saori Isikawa <saori.isikawa414@gmail.com>
2025-01-25 03:54:19 +0800

    update

--- bin/update.sh
+++ bin/update.sh
@@ -4,8 +4,7 @@
 sudo apt update && sudo apt upgrade -y
 sudo snap refresh
 sudo flatpak -y update
-#fc-cache -fv
-#sudo fc-cache -fv
+sudo fc-cache -fv
 #sudo update-initramfs -c -k "$(uname -r)"
 #sudo update-grub
 
