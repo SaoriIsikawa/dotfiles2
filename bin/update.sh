78a8f210fe4f5bdbb780ed2fcf37da2b75b99fb0
Saori Isikawa <saori.isikawa414@gmail.com>
2025-02-19 05:29:47 +0800

    update

--- bin/update.sh
+++ bin/update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#sudo ntpd -qg && sudo hwclock -w -v
-#sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 sudo apt update && sudo apt upgrade -y
+sudo apt autoremove
 sudo snap refresh
 flatpak --user -y update
 #sudo fc-cache -fv
